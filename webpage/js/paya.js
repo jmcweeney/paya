97b2c5a82572bb66bd4c3c8e616e4004e79ab65e
Jeff McWeeney <jeff@addl.co>
2018-12-12 10:41:09 -0500

    Updated define your business, tell us about your business, added file-upload js

--- webpage/js/paya.js
+++ webpage/js/paya.js
@@ -116,3 +116,17 @@ $('.carousel').carousel({
   interval: false,
   wrap: false
 });
+
+// File Upload
+function readUrl(input) {
+  if (input.files && input.files[0]) {
+    let reader = new FileReader();
+    reader.onload = (e) => {
+      let imgData = e.target.result;
+      let imgName = input.files[0].name;
+      input.setAttribute("data-title", imgName);
+      console.log(e.target.result);
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
